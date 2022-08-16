35dfa8d5b99a15cd47905a131f6e56379c3e34e7
barbarazamperete <bahzamp25@gmail.com>
2022-08-16 09:57:36 -0400

    Alterando a forma do Form Control

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { NonNullableFormBuilder} from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from './../services/courses.service';
@@ -12,19 +12,17 @@ import { CoursesService } from './../services/courses.service';
 })
 export class CourseFormComponent implements OnInit {
 
-  form: FormGroup //grupo de compos
+  form = this.formBuilder.group({
+    name: [''], //inicializa o campo sem valor
+    category: ['']
+  }) //grupo de compos
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: NonNullableFormBuilder,
     private service: CoursesService,
     private _snackBar: MatSnackBar,
     private location: Location
-  ) {
-    this.form = this.formBuilder.group({
-      name: [null], //inicializa o campo sem valor
-      category: [null]
-    })
-  }
+  ) { }
 
   ngOnInit(): void {
     // TODO document why this method 'ngOnInit' is empty
@@ -40,7 +38,7 @@ export class CourseFormComponent implements OnInit {
     this.location.back()
   }
 
-  private onSuccess(){
+  private onSuccess() {
     this._snackBar.open("Curso salvo com sucesso", '', {
       duration: 5000
     })
